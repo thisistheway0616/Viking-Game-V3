5739874e1a2dbe150b7ff2d22e5bd90ef10cf66a
Eric <ebseel95@gmail.com>
2026-03-06 17:49:15 -0600

    Tweaked MC Script

--- game-client/scripts/player.gd
+++ game-client/scripts/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -850.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
