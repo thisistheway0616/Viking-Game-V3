70c085ade9e34770e6b13093b310da12e5f908cf
Eric <ebseel95@gmail.com>
2026-03-06 19:44:34 -0600

    Added attack animations and logic

--- game-client/scripts/player.gd
+++ game-client/scripts/player.gd
@@ -4,24 +4,48 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -600.0
+var attack_hold_timer: float = 0.0
+const HEAVY_ATTACK_THRESHOLD: float = 0.3
+var is_charging_attack: bool = false
+var is_attacking: bool = false
 
 
 func _physics_process(delta: float) -> void:
-	
-	# Add animation
-	if velocity.x > 1 or velocity.x < -1:
-		animated_sprite_2d.animation = "walk"
-	else:
-		animated_sprite_2d.animation = "idle"
-	# Add the gravity.
+	if not is_attacking:
+		if not is_on_floor():
+			animated_sprite_2d.play("jump")
+		elif abs(velocity.x) > 1:
+			animated_sprite_2d.play("walk")
+		else:
+			animated_sprite_2d.play("idle")
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animated_sprite_2d.animation = "jump"
+	# Add animation
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#Attacking
+	#Left Mouse Click Hold Logic
+	if Input.is_action_just_pressed("light_attack"):
+		is_charging_attack = true
+		attack_hold_timer = 0.0
+	
+	if is_charging_attack:
+		attack_hold_timer += delta
+		print("Timer: ", attack_hold_timer)
+		
+		if Input.is_action_just_released("light_attack"):
+			print("Realeased! Final Time: ", attack_hold_timer)
+			if attack_hold_timer >= HEAVY_ATTACK_THRESHOLD:
+				perform_heavy_attack()
+			else:
+				perform_light_attack()
+			is_charging_attack = false
+	
+	if Input.is_action_just_released("light_attack") and is_charging_attack and not is_attacking:
+		perform_light_attack()
+		is_charging_attack = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -36,3 +60,15 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.flip_h = false
 	elif direction == -1.0:
 		animated_sprite_2d.flip_h = true
+func perform_light_attack():
+	is_attacking = true
+	print("Light Attack Triggered")
+	animated_sprite_2d.play("light_attack")
+	await animated_sprite_2d.animation_finished
+	is_attacking = false
+func perform_heavy_attack():
+	is_attacking = true
+	print("Heavy Attack Triggered")
+	animated_sprite_2d.play("heavy_attack")
+	await animated_sprite_2d.animation_finished
+	is_attacking = false
